41ca4254f883fda1651f040843ea36a8751b6178
Chloé Cilia <chloe.cilia68@gmail.com>
2024-12-14 23:25:49 +0100

    fix rqst n.17 & add rqst n.19

--- Requête #17.sql
+++ Requête #17.sql
@@ -2,4 +2,4 @@ Requête #17
 
 SELECT *,RECIPE.recipe_name FROM compose
 INNER JOIN RECIPE ON compose.id_recipe = RECIPE.id_recipe
-WHERE compose.id_ingredient IS NULL
+WHERE EXISTS (compose.id_ingredient IS NULL)
---
+++ Requête #19.sql
@@ -0,0 +1,2 @@
+INSERT INTO INGREDIENT(INGREDIENT.id_ingredient,INGREDIENT.measurementUnit,INGREDIENT.name_ingredient,INGREDIENT.price)
+VALUES(100,'gramme','caviar',1000)
