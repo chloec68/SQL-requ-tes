cc14b6b86747db7a75d5e85eff711885852a23b0
Chloé Cilia <chloe.cilia68@gmail.com>
2024-12-14 22:24:27 +0100

    add request n.13

---
+++ Requête#13.sql
@@ -0,0 +1,3 @@
+SELECT INGREDIENT.name_ingredient,INGREDIENT.id_ingredient,compose.id_recipe FROM INGREDIENT 
+INNER JOIN compose on INGREDIENT.id_ingredient = compose.id_ingredient
+WHERE INGREDIENT.name_ingredient LIKE '%poulet%'
