e4bfd6ae3225462c578cd8926afa7f0b2c171e4d
Chloé Cilia <chloe.cilia68@gmail.com>
2024-12-18 11:47:39 +0100

    correction

--- Requête #6.sql
+++ Requête #6.sql
@@ -1,1 +1,5 @@
+ Modifier le nom de la recette ayant comme identifiant id_recette = 3 (nom de la recette à votre
+convenance)
+
+
 UPDATE recipe SET recipe_name = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" WHERE id_recipe = 16
